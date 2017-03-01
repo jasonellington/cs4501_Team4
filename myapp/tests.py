f7c0a010903f226c037d25cf5ea7a273d23f1e30
jzimmerman95 <jessicazimmerman95@gmail.com>
2017-03-01 11:12:43 -0500

    initial unit tests

--- myapp/tests.py
+++ myapp/tests.py
@@ -1,3 +1,32 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.core.urlresolvers import reverse
+from myapp.models import Car, User
 
-# Create your tests here.
+class GetCarDetailsTestCase(TestCase):
+	fixtures = ['db.json']
+
+    #setUp method is called before each test in this class
+	def setUp(self):
+		pass #nothing to set up
+
+	def test_get_cars_success(self):
+		url = reverse('myapp:get_cars')
+		response = self.client.get(url).json()
+
+		self.assertEquals(response['ok'], True)
+
+	def test_get_car_success(self):
+		url = reverse('myapp:get_car', kwargs={'car_id':3})
+		response = self.client.get(url).json()
+
+		self.assertEquals(response['ok'], True)
+
+	def test_get_car_fail(self):
+		url = reverse('myapp:get_car', kwargs={'car_id':0})
+		response = self.client.get(url).json()
+
+		self.assertEquals(response['ok'], False)
+
+    #tearDown method is called after each test
+	def tearDown(self):
+		pass #nothing to tear down
