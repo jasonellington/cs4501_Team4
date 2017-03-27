ccae20cda85e90cac209b653f19ea6ede761b246
abp5fn <abp5fn@virginia.edu>
2017-03-27 11:03:54 -0400

    commit for merge

--- web/web/views.py
+++ web/web/views.py
@@ -5,7 +5,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render, render_to_response
 from django.http import HttpResponse, HttpResponseRedirect
 import requests
-from .forms import RegisterForm, NewListingForm
+from .forms import RegisterForm, NewListingForm, LoginForm
 
 # Create your views here.
 
@@ -32,7 +32,27 @@ def register(request):
 	return render(request, 'web/register.html', {'form': register_form, 'user_id': user_id, 'password':password})
 
 def login(request):
-	return render(request, 'web/login.html')
+	form_class = LoginForm
+
+	if request.method == 'POST':
+		form = LoginForm(request.POST)
+
+		if form.is_valid():
+			post_data = {
+            'user_id': form.cleaned_data['user_id'],
+            'password': form.cleaned_data['password'],
+      }
+			#r = requests.post('http://exp-api:8000/exp/create/listing', post_data)
+			return render(request, 'web/logged_in.html', {'post_data': post_data})
+
+		else:
+			form = LoginForm()
+
+	return render(request, 'web/login.html', {'form': form_class})
+
+def logged_in(request):
+
+	return render(request, 'web/logged_in.html')
 
 def create_listing(request):
 	form_class = NewListingForm
