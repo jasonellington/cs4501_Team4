9a3528f76802f6d12f718bfe091bdcac91c1d83f
abp5fn <abp5fn@virginia.edu>
2017-04-11 17:23:59 -0400

    fix load fixtures

--- models/load_fixtures.py
+++ models/load_fixtures.py
@@ -1,7 +1,17 @@
 from elasticsearch import Elasticsearch
 import json
+from .models import Car
 
-es = Elasticsearch(['es'])
+
+retry = True
+
+while retry:
+    try:
+		es = Elasticsearch(['es'])
+    except:
+        print("Exception. Retrying..")
+    else:
+        retry = False
 
 with open('db.json') as fixture_file:
 	response = fixture_file
@@ -9,20 +19,12 @@ with open('db.json') as fixture_file:
 
 	for i in json_obj:
 		if i['model'] == 'models.car':
-			make = i['fields']['make']
-			car_model = i['fields']['car_model']
-			year = i['fields']['year']
-			body_type = i['fields']['body_type']
-			num_seats = i['fields']['num_seats']
 			date_created = i['fields']['date_created']
 
-			some_new_listing = {'make': make, 'car_model': car_model, 'year': year, 'body_type': body_type, 'num_seats': num_seats, 'date_created':date_created, 'id':date_created}
+			car = Car.objects.get(date_created=date_created)
 
-			es.index(index='listing_index', doc_type='listing', id=some_new_listing['id'], body=some_new_listing)
+			new_listing = {'make': car.make, 'car_model': car.car_model, 'year': car.year, 'body_type': car.body_type, 'num_seats': car.num_seats, 'date_created':car.date_created, 'id':car.id}
 
-			es.indices.refresh(index="listing_index")
+			es.index(index='listing_index', doc_type='listing', id=new_listing['id'], body=new_listing)
 
-
-
-
-#TODO: In the compose file, for models container, add the command python load_fixtures.py between the loaddata command and the server command
+es.indices.refresh(index="listing_index")
