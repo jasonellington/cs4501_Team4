ea9acd4ca735dd41941ce7553c0feffa1b90d848
Jason Ellington <jte4hm@virginia.edu>
2017-04-12 16:55:39 -0400

    fix register

--- web/web/forms.py
+++ web/web/forms.py
@@ -5,19 +5,9 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class RegisterForm(forms.Form):
     user_id = forms.CharField(label='Username', max_length=100)
     password = forms.CharField(label='Password', max_length=100, widget=forms.PasswordInput)
-    c_password = forms.CharField(label='Confirm Password', max_length=100, widget=forms.PasswordInput)
     first_name = forms.CharField(label='First Name', max_length=100)
     last_name = forms.CharField(label='Last Name', max_length=100)
     age = forms.IntegerField(label='Age', validators=[MaxValueValidator(100), MinValueValidator(1)])
-
-    def clean_password(self):
-        password1 = self.cleaned_data.get('password')
-        password2 = self.cleaned_data.get('c_password')
-
-        if password1 and password1 != password2:
-            raise forms.ValidationError("Passwords don't match")
-
-        return self.cleaned_data
 
 
 class NewListingForm(forms.Form):
