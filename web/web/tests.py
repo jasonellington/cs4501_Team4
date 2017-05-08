cc7584459f854b212fc9b7832d9f6bdab8362c09
abp5fn <abp5fn@virginia.edu>
2017-05-08 17:58:47 -0400

    selenium updates

--- web/web/tests.py
+++ web/web/tests.py
@@ -6,27 +6,26 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 class SeleniumTests(TestCase):
 
     def setUp(self):
-        self.driver = webdriver.Remote(
+        self.selenium = webdriver.Remote(
             command_executor='http://selenium_chrome:4444/wd/hub/',
             desired_capabilities=DesiredCapabilities.CHROME
         )
 
     def test_register_user(self):
-        driver = self.driver
-        resp = driver.get("http://load_balancer:8005/register")
-        print(driver.title)
-        username = driver.find_element_by_id("id_user_id")
+        selenium = self.selenium
+        resp = selenium.get("http://172.17.0.10:8000/register")
+        username = selenium.find_element_by_id('id_user_id')
         username.send_keys("jkz3km")
-        password = driver.find_element_by_id("id_password")
+        password = selenium.find_element_by_id("id_password")
         password.send_keys("gohoos")
-        fname = driver.find_element_by_id("id_first_name")
+        fname = selenium.find_element_by_id("id_first_name")
         fname.send_keys("Jessica")
-        lname = driver.find_element_by_id("id_last_name")
+        lname = selenium.find_element_by_id("id_last_name")
         lname.send_keys("Zimmerman")
-        age = driver.find_element_by_id("id_age")
+        age = selenium.find_element_by_id("id_age")
         age.send_keys("22")
-        driver.find_element_by_id("submit").click()
-        assert "Thanks for registering!" in driver.page_source
+        selenium.find_element_by_id("submit").click()
+        assert "http://172.17.0.10:8000/register" in selenium.current_url
 
     def tearDown(self):
-        self.driver.close()
+        self.selenium.close()
